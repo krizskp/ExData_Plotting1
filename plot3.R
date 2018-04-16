c8b1b0eeb97cb8eb91b3871a55ade2865162007b
Shalvindra K. Prasad <shalvindra@pacefiji.com>
2018-04-16 16:23:59 +1200

    Corrected graph for plot 3

--- plot3.R
+++ plot3.R
@@ -5,6 +5,10 @@ dataSubset <- subset(dataFile, subset = (Date >= "2007-02-01" & Date <= "2007-02
 dataSubset$DateTime <- strptime(paste(dataSubset$Date, dataSubset$Time), "%Y-%m-%d %H:%M:%S")
 dataSubset$DateTime <- as.POSIXct(dataSubset$DateTime)
 
-plot(Global_active_power ~ dataSubset$DateTime, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
-dev.copy(png, file = "plot2.png", height = 480, width = 480)
+plot(Sub_metering_1 ~ dataSubset$DateTime, type = "l", ylab = "Energy sub metering", xlab = "")
+lines(Sub_metering_2 ~ dataSubset$DateTime, col = "Red")
+lines(Sub_metering_3 ~ dataSubset$DateTime, col = "Blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+dev.copy(png, file = "plot3.png", height = 480, width = 480)
 dev.off()
